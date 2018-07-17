14bd3fde27028ca8d82111056cec5fcb012ada7a
enovasi <36550853+enovasi@users.noreply.github.com>
2018-07-16 23:17:53 -0500

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -3,11 +3,11 @@
     'name': "Enovasi",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        # Short (1 phrase/line) summary of the module's purpose, used as
+        # subtitle on modules listing or apps.openerp.com""",
 
     'description': """
-        Long description of module's purpose
+        # Long description of module's purpose
     """,
 
     'author': "Enovasi",
